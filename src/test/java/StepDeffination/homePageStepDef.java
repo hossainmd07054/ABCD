cc6d7da6f999f0591dd568187f040a93b6d5ead7
hossain07054 <hossain07054@gmail.com>
2024-11-22 21:18:39 -0500

    fixt

--- src/test/java/StepDeffination/homePageStepDef.java
+++ src/test/java/StepDeffination/homePageStepDef.java
@@ -1,23 +1,25 @@
 package StepDeffination;
+
 import PageAction.homePageAction;
 import Util.testBase;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 
 public class homePageStepDef extends testBase {
 	homePageAction homepageaction = new homePageAction();
 
-	@Given("^Open Application \"([^\"]*)\"$")
-	public void open_Application(String URL) throws Throwable {
+	@Given("Open Application {string}")
+	public void open_application(String URL) {
 		launch(URL);
 	}
 
-	@Then("^Click on Login Button$")
-	public void click_on_Login_Button() throws Throwable {
+	@Then("Click on Login button")
+	public void click_on_login_button() throws Throwable {
 		homepageaction.ClickonLoginButton();
-	    
 	}
-	@Then("^Enter username and Pass$")
-	public void enter_username_and_Pass() throws Throwable {
-	}  
+
+	@Then("Enter username and Pass")
+	public void enter_username_and_pass() {
+
+	}
 }
